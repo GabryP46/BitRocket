f9e6873566ed8bcbdf9f4ef79cc7efc3b13983e6
Gabriele Polizzotto <>
2021-02-01 17:59:00 +0100

    Print the APIs and show a clear design

---
+++ not-react/posts.js
@@ -0,0 +1,8 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(posts => {
+    var str = '<ul>';
+    posts.forEach(post =>  str += '<li>'+ post.title + '</li>' + '</br>' + '<li>'+ post.body + '</li>' + '</br>' + '<hr />' + '</br></br>');
+    str += '</ul>';
+    document.getElementById("slideContainer").innerHTML = str;
+})
